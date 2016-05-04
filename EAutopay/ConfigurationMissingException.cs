38d95d2387be01406d4e5a71e9ec2646e0ab92b7
Ivan Studenikin <515516@gmail.com>
2016-05-04 16:29:46 +0400

    added new exception class

---
+++ EAutopay/ConfigurationMissingException.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+
+namespace EAutopay
+{
+    /// <summary>
+    /// Encapsulates exception that is thrown if configuration file missing required element(s).
+    /// </summary>
+    public class ConfigurationMissingException : ConfigurationErrorsException
+    {
+        /// <summary>
+        /// Name of the missing element in the configuration file.
+        /// </summary>
+        public string MissingElement { get; private set; }
+
+        /// <summary>
+        /// Initializes instance of the object.
+        /// </summary>
+        /// <param name="missingElement">Missing element name.</param>
+        public ConfigurationMissingException(string missingElement) : base(missingElement)
+        {
+            MissingElement = missingElement;
+        }
+    }
+}
