fba60c08823e14ec29627c3c7df2aba9c24228fc
Ivan Studenikin <515516@gmail.com>
2016-05-11 19:22:23 +0400

    unit-tests: ProductParser

---
+++ EAutopay.Tests/EAutopayProductParserTest.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using EAutopay.Parsers;
+
+namespace EAutopay.Tests
+{
+    [TestClass]
+    public class EAutopayProductParserTest
+    {
+        [TestMethod]
+        public void ProductParser_Returns_Correct_List_Of_Products()
+        {
+            string  source = File.ReadAllText("../../Data/Products.html");
+
+            var parser = new EAutopayProductParser();
+            var products = parser.ExtractProducts(source);
+
+            Assert.AreEqual(3, products.Count);
+
+            var p = products
+                .Where(prod => !prod.IsUpsell)
+                .OrderByDescending(prod => prod.ID)
+                .First();
+
+            Assert.AreEqual(235044, p.ID);
+            Assert.AreEqual("Товар 2", p.Name);
+            Assert.AreEqual(1400.00, p.Price);
+        }
+    }
+}
