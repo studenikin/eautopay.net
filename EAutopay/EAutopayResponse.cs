18420b37edb31e9b2b911ca6c69eb1e71307fdc0
Ivan Studenikin <515516@gmail.com>
2016-05-16 06:57:57 +0400

    annotation made for the EAutopayResponse class

--- EAutopay/EAutopayResponse.cs
+++ EAutopay/EAutopayResponse.cs
@@ -3,12 +3,22 @@ using System.Net;
 
 namespace EAutopay
 {
+    // Encapsulates the HTTP response from E-Autopay.
     public class EAutopayResponse
     {
+        /// <summary>
+        /// Gets the source of the HTTP response.
+        /// </summary>
         public string Data { get; internal set; }
 
+        /// <summary>
+        /// Get the URI of the HTTP response.
+        /// </summary>
         public Uri Uri { get; internal set; }
 
+        /// <summary>
+        /// Gets the status code of the HTTP response.
+        /// </summary>
         public HttpStatusCode StatusCode { get; internal set; }
     }
 } 
